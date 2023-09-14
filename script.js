7373656b9d1b7e8ce8601e55e4d019534e21d0ac
Charandeep Singh <charandeeps81@gmail.com>
2023-09-14 21:27:00 +0530

    Added drawball in javacript

--- script.js
+++ script.js
@@ -8,6 +8,8 @@
 //     }
 // }
 
+// Html Section
+
 function setRandomCornerBorder(el) {
     const corner = Math.floor(Math.random() * 4);
     
@@ -43,12 +45,14 @@ function generateHtmlPattern(id) {
             hue: 'blue'
         });
 
-        const randInt = getRandomInt(4);
+        const randInt = getRandomInt(20);
 
-        if (randInt === 0) {
+        if (randInt < 3) {
             el.classList.add('hoverRotateLeft');
-        } else if (randInt === 1) {
+        } else if (randInt < 6) {
             el.classList.add('hoverRotateRight');
+        } else if (randInt < 9) {
+            el.classList.add('rotateAni');
         }
         
         el.style.backgroundColor = color;
@@ -60,3 +64,65 @@ function generateHtmlPattern(id) {
 }
 
 generateHtmlPattern('htmlpat');
+
+// JavaScript Section
+
+function drawBounce() {
+
+    var canvas = document.getElementById("can");
+    var ctx = canvas.getContext("2d");
+    var ballRadius = 10;
+    var x = canvas.width / 2;
+    var y = canvas.height - 30;
+    var dx = 2;
+    var dy = -1;
+    var paddleHeight = 10;
+    var paddleWidth = 75;
+    var paddleX = (canvas.width - paddleWidth) / 2;
+
+
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+    function drawPaddle(x, y) {
+        ctx.beginPath();
+        ctx.rect(x, y, paddleHeight, paddleWidth);
+        ctx.fillStyle = "#009500";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall();
+
+        if (y > canvas.height - paddleWidth / 2) {
+            drawPaddle(0, canvas.height - paddleWidth);
+            drawPaddle(canvas.width - 10, canvas.height - paddleWidth);
+        } else if (y < paddleWidth / 2) {
+            drawPaddle(0, 0);
+            drawPaddle(canvas.width - 10, 0);
+        } else {
+            drawPaddle(0, y - paddleWidth / 2);
+            drawPaddle(canvas.width - 10, y - paddleWidth / 2);
+        }
+
+        if (x + 5 + dx > canvas.width - ballRadius || x - 5 + dx < ballRadius) {
+            dx = -dx;
+        }
+        if (y + dy > canvas.height - ballRadius || y - 5 + dy < ballRadius) {
+            dy = -dy;
+        }
+
+        x += dx;
+        y += dy;
+    }
+
+    setInterval(draw, 10);
+}
+
+drawBounce();
