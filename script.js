ba4c57c1e5542ca5e51b81b4d48fd607e0fa14b8
Charandeep Singh <charandeeps81@gmail.com>
2024-01-03 21:32:41 +0530

    Fixed some UI and dark mode

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function showNav() {
         navContext.style.display = "block";
     }
 }
+
+function comaToHlList() {
+    const comaElements = document.getElementsByClassName("comaToHlList");
+    for (const elem of comaElements) {
+
+        const listOfWords = elem.textContent.split(",");
+        elem.innerHTML = "";
+        for (const word of listOfWords) {
+            const newElem = document.createElement("div");
+            const newContent = document.createTextNode(word.trim());
+            newElem.appendChild(newContent);
+            elem.appendChild(newElem);
+        }
+    }
+}
+
+comaToHlList();
--- theme.js
+++ theme.js
@@ -4,18 +4,32 @@ function darkMode(isOn) {
     if (isOn) {
         document.documentElement.style.setProperty('--font-color', 'white');
         document.documentElement.style.setProperty('--background-color', '#121212');
+    
+        document.documentElement.style.setProperty('--font-back-color', '#4c2f2f');
+
+        document.documentElement.style.setProperty('--link-color', 'rgb(0, 194, 0)');
         return;
     }
     
     document.documentElement.style.setProperty('--font-color', 'black');
     document.documentElement.style.setProperty('--background-color', 'white');
+
+    document.documentElement.style.setProperty('--font-back-color', '#e8dddd');
+    
+    document.documentElement.style.setProperty('--link-color', 'green');
 }
 
-// Theme change event listener
-darkThemeMq.addEventListener("change", e => {
-    if (e.matches) {
+function checkAndSetDarkMode(mode) {
+    if (mode) {
         darkMode(true);
     } else {
         darkMode(false);
     }
+}
+
+checkAndSetDarkMode(darkThemeMq.matches);
+
+// Theme change event listener
+darkThemeMq.addEventListener("change", e => {
+    checkAndSetDarkMode(darkThemeMq.matches);
 });
